866e096b766f7e8ca31a88455f6bcc9dd9e79ce3
olaide-hok <olaidekareem43@yahoo.com>
2022-09-04 07:41:47 +0100

    Setup Context with custom hook.

--- src/context.js
+++ src/context.js
@@ -1,1 +1,18 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+    return (
+        <AppContext.Provider value='hello'>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+// Custom Hook
+export const useGlobalContent = () => {
+    return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
